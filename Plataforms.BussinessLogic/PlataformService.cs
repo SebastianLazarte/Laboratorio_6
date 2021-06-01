e9e05d2835a482f8d8052fa8bac388c61615e20b
SebastianLazarte <72448046+SebastianLazarte@users.noreply.github.com>
2021-06-01 18:38:23 -0400

    err

--- Plataforms.BussinessLogic/PlataformService.cs
+++ Plataforms.BussinessLogic/PlataformService.cs
@@ -1,4 +1,6 @@
-public class PlataformService : IPlataformService
+namespace Plataforms.BussinessLogic
+{
+    public class PlataformService : IPlataformService
     {
         public string GetPlataformAsync(int id){return "";}
         public string GetPlataformsAsync(string orderBy = "id"){return "";}
@@ -7,3 +9,4 @@ public class PlataformService : IPlataformService
         public string DeletePlataformAsync(int id){return "";}
     
     }
+}
